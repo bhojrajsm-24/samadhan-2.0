244466bbe29194187e348d7ea49327397d8c1ffb
bhojrajsm-24 <bhojrajsm24@gmail.com>
2025-08-24 23:59:55 +0530

    Day-1.js

---
+++ Day-1.js
@@ -0,0 +1,18 @@
+// Day-1 Task: Print student details from an object
+
+// Student object
+const student = {
+    name: "Raju rastogi",
+    age: 20,
+    course: "B.Tech Computer Science",
+    rollNo: "CS2025",
+    college: "ABC Institute of Technology"
+};
+
+// Print details
+console.log("Student Details:");
+console.log("Name: " + student.name);
+console.log("Age: " + student.age);
+console.log("Course: " + student.course);
+console.log("Roll No: " + student.rollNo);
+console.log("College: " + student.college);
