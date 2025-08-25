93e111599efc2c6def8c969b69dc6608fa1bb014
bhojrajsm-24 <bhojrajsm24@gmail.com>
2025-08-25 22:50:18 +0530

    Day-3.js

---
+++ Day-3.js
@@ -0,0 +1,13 @@
+//task-3 of hackathon......
+let studentName = "oggy";
+let marks1 = 75;
+let marks2 = 82;
+let marks3 = 68;
+
+let total = marks1 + marks2 + marks3;
+let average = total / 3;
+
+console.log("Student Name: " + studentName);
+console.log("Total Marks: " + total);
+console.log("Average Marks: " + average);
+
